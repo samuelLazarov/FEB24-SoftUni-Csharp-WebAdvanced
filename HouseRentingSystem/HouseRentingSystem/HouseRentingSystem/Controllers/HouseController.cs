31c3445108b146cb02c02a2b9233da0976bce92b
SamuelLazarov <samuel.lazarov@gmail.com>
2024-02-26 18:50:00 +0200

    Controllers and views added

---
+++ HouseRentingSystem/HouseRentingSystem/HouseRentingSystem/Controllers/HouseController.cs
@@ -0,0 +1,60 @@
+﻿using HouseRentingSystem.Core.Models.House;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HouseRentingSystem.Controllers
+{
+    [Authorize]
+    public class HouseController : Controller
+    {
+        [AllowAnonymous]
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var model = new AllHousesQueryModel();
+            
+            return View(model);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Mine()
+        {
+            var model = new AllHousesQueryModel();
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Details(int id)
+        {
+            var model = new HouseDetailsViewModel();
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(HouseFormModel model)
+        {
+            return RedirectToAction(nameof(Details), new {id =1 });
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id) 
+        {
+            var model = new HouseFormModel();
+			return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, HouseFormModel house)
+        {
+            return RedirectToAction(nameof(Details), new { id = "1" });
+        }
+    }
+}
--- HouseRentingSystem/HouseRentingSystem/HouseRentingSystem/Extensions/ServiceCollectionExtension.cs
+++ HouseRentingSystem/HouseRentingSystem/HouseRentingSystem/Extensions/ServiceCollectionExtension.cs
@@ -25,7 +25,14 @@ public static class ServiceCollectionExtension
     public static IServiceCollection AddApplicationIdentity(this IServiceCollection services, IConfiguration config)
     {
         services
-            .AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            .AddDefaultIdentity<IdentityUser>(options =>
+            { 
+                options.SignIn.RequireConfirmedAccount = false;
+                options.Password.RequireDigit = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+            })
             .AddEntityFrameworkStores<HouseRentingDbContext>();
 
         return services;
