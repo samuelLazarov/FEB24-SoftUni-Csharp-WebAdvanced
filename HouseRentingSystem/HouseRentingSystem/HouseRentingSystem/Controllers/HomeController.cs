e1d841480cef45a131a197d871153ef2b1f11216
SamuelLazarov <samuel.lazarov@gmail.com>
2024-03-03 12:17:42 +0200

    Repository added

--- HouseRentingSystem/HouseRentingSystem/HouseRentingSystem/Controllers/HomeController.cs
+++ HouseRentingSystem/HouseRentingSystem/HouseRentingSystem/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using HouseRentingSystem.Core.Models.Home;
+﻿using HouseRentingSystem.Core.Contracts.House;
+using HouseRentingSystem.Core.Models.Home;
 using HouseRentingSystem.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
@@ -7,16 +8,20 @@ namespace HouseRentingSystem.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly ILogger<HomeController> logger;
+        private readonly IHouseService houseService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(
+            ILogger<HomeController> _logger,
+            IHouseService _houseService)
         {
-            _logger = logger;
+            logger = _logger;
+            houseService = _houseService;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            var model = new IndexViewModel();
+            var model = await houseService.LastThreeHouses();
             return View(model);
         }
 
